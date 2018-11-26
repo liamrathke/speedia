0a53dfce1f5b5a0bdd2a603a2943ac618b8998b2
liamrathke <liam.rathke@gmail.com>
2018-11-25 20:28:48 -0500

    Adds UI outline for home screen
    Contains placeholder images, CSS styling yet to be done

--- src/router.js
+++ src/router.js
@@ -8,11 +8,7 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [{
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
-  }, {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
   }]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <br>
+        <div class="row">
+            <div class="col-sm-8 col-md-6 col-lg-4 offset-sm-2 offset-md-3 offset-lg-4">
+                <div class="input-group text-center">
+                    <input type="text" class="w-100" v-model="name" placeholder="Username">
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 col-md-10 offset-md-1">
+                <div class="row">
+                    <div class="col-6 col-lg-4" v-for="category in categories" v-bind:key="category.name">
+                        <div class="card">
+                            <img class="card-img-top" v-bind:src="category.image">
+                            <div class="card-body">
+                                <p class="card-text">{{category.name}}</p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-md-4 offset-3 offset-md-4">
+                <button type="button" class="btn btn-primary btn-lg w-100">Go!</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data: function() {
+    return {
+      name: '',
+      selectedCategory: 'Grab Bag',
+      categories: [
+        {name: 'Grab Bag', image: require('../assets/grab-bag.png')},
+        {name: 'History', image: require('../assets/history.png')},
+        {name: 'News', image: require('../assets/news.png')},
+        {name: 'Geography', image: require('../assets/geography.png')},
+        {name: 'Science', image: require('../assets/science.png')},
+        {name: 'Culture', image: require('../assets/culture.png')}
+      ]
+    }
   }
 }
 </script>
