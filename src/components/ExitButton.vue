00a312e03c32a14ec03369db4ba517933e5a8291
Liam Rathke <33555592+liamrathke@users.noreply.github.com>
2019-03-09 19:17:55 -0500

    Adds exit button
    Not tested yet

---
+++ src/components/ExitButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="exit-button-container">
+        <button type="button" class="btn btn-lg btn-danger w-100 exit-button drop-shadow b-0" v-bind:disabled="!enabled" v-on:click="$emit('exit', true)">Exit</button>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    enabled: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
