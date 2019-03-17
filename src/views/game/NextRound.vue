b352a14c9970262caa52fea87bb007c65176e338
Liam Rathke <33555592+liamrathke@users.noreply.github.com>
2019-03-17 15:12:52 -0400

    Fixes some more CSS formatting
    Not done yet

--- src/views/game/NextRound.vue
+++ src/views/game/NextRound.vue
@@ -1,24 +1,31 @@
 <template>
-    <div class="dark-bg text-center center-parent">
-        <article-title flag="GOAL" v-bind:text="roundInfo.end"></article-title>
+    <div class="dark-bg text-center center-parent ai-end">
+        <div class="goal-text">
+            <article-title flag="GOAL" v-bind:text="roundInfo.end"></article-title>
+        </div>
         <div class="container container-mw-lg">
-            <h1 class="game-text game-found-text m-0 mb-4"><em>Round {{roundInfo.currentRound}}!</em></h1>
-            <div>
-                <user-intro v-bind:user="gameUsers[0]"></user-intro>
-            </div>
-            <div class="my-5">
-                <hr class="line-break-white">
-            </div>
-            <div>
-                <user-intro v-bind:user="gameUsers[1]"></user-intro>
+            <div class="center-parent">
+                <h1 class="game-text game-found-text m-0 mb-4"><em>Round {{roundInfo.currentRound}}!</em></h1>
+                <div class="center-parent">
+                    <user-update v-bind:user="gameUsers[0]"></user-update>
+                    <div class="my-2">
+                        <hr class="line-break-white start-edge">
+                    </div>
+                    <user-update v-bind:user="gameUsers[1]"></user-update>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import UserUpdate from './NextRound/UserUpdate.vue'
+
 export default {
   name: 'NextRound',
+  components: {
+    UserUpdate
+  },
   props: {
     roundInfo: {
       type: Object,
---
+++ src/views/game/NextRound/UserUpdate.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row w-100">
+        <div class="col-10 col-md-6 col-lg-4 offset-1 offset-md-3 offset-lg-4">
+            <user-intro v-bind:user="user"></user-intro>
+            <h3 class="text-white">
+                <span class="mr-1">is at</span>
+                <span>{{user.lastArticle}}</span>
+            </h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserUpdate',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
