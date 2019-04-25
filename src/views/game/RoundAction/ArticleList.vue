512b4284d5c6b80870d42e42e92f3d0ddc3a2488
liamrathke <liam.rathke@gmail.com>
2019-04-25 16:41:38 -0400

    Begins work on article list
    Not finalized or centered properly yet

--- src/views/game/RoundAction/ArticleList.vue
+++ src/views/game/RoundAction/ArticleList.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        List Stuff
+    <div class="container container-mw-lg">
+        <div v-for="(article, articleIndex) in articles" v-bind:key="article">
+            <article-button v-bind:articleTitle="article" v-bind:selected="false"></article-button>
+        </div>
     </div>
 </template>
 
 <script>
+import ArticleButton from './ArticleList/ArticleButton.vue'
+
 export default {
   name: 'ArticleList',
+  components: {
+    ArticleButton
+  },
   props: {
     articles: {
       type: Array,
---
+++ src/views/game/RoundAction/ArticleList/ArticleButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="standout-card card drop-shadow w-100">
+        <p class="card-text w-100 text-center w-100">{{articleTitle}}</p>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'ArticleButton',
+  props: {
+    articleTitle: {
+      type: String,
+      required: true
+    },
+    selected: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
