271f5bd857a61ddc95a02a2c94fa2ab0b7acdad1
liamrathke <liam.rathke@gmail.com>
2019-04-26 14:57:09 -0400

    Emits article selected event
    Also handled by backend properly??

--- src/views/game/RoundAction.vue
+++ src/views/game/RoundAction.vue
@@ -2,7 +2,7 @@
     <div>
         <countdown end="TEST"></countdown>
         <article-search searchString="TEST"></article-search>
-        <article-list v-bind:selectedArticle="selectedArticle" v-bind:articles="actionInfo.articles" v-on:select="selectedArticle = $event"></article-list>
+        <article-list v-bind:selectedArticle="selectedArticle" v-bind:articles="actionInfo.articles" v-on:select="selectArticle($event)"></article-list>
     </div>
 </template>
 
@@ -21,6 +21,12 @@ export default {
   data: function() {
     return {
       selectedArticle: this.actionInfo.selectedArticle
+    }
+  },
+  methods: {
+    selectArticle(article) {
+      this.$socket.emit('selectArticle', article)
+      this.selectedArticle = article
     }
   }
 }
