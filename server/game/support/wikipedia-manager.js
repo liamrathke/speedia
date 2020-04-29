927e0005cac3870aefc479258b86d80a9c7d7969
Liam Rathke <33555592+liamrathke@users.noreply.github.com>
2020-04-28 22:19:37 -0400

    Fixes Wikipedia access issue
    Adds header to Wikipedia API requests

--- server/game/support/wikipedia-manager.js
+++ server/game/support/wikipedia-manager.js
@@ -1,8 +1,10 @@
 let Wikipedia = require('wikijs').default
 
+let headers = { 'User-Agent': 'Speedia (https://github.com/liamrathke/speedia; larathke@wpi.edu) wiki.js' }
+
 module.exports = class WikipediaManager {
   static async getLinks(title) {
-    let page = await Wikipedia().find(title)
+    let page = await Wikipedia({headers}).find(title)
     return page.links()
   }
 }
